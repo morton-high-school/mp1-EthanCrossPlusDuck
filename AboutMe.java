ef6141e5f2a0bc3af6d0fd22162ce62dfd8ab80d
Ethan Cross <ethan.cross@mcusd709.org>
2024-08-21 09:07:35 -0500

    Fixed errors and added an AboutMe class.

---
+++ AboutMe.java
@@ -0,0 +1,7 @@
+public class AboutMe{
+    public static void main(String[] args){
+        System.out.println("My name is Ethan.");
+        System.out.println("I am a junior.");
+        System.out.println("I like to run.");
+    }
+}
